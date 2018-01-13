6f4c14c7fea13ccdeebb88c36fa2aa6469b37ae1
hoital <hoital79@gmail.com>
2018-01-13 17:10:58 +0900

    init

---
+++ script.js
@@ -0,0 +1,20 @@
+function translator(str) {
+  return str.split('').map(function(char) {
+    var d = Hangul.disassemble(char);
+    console.log(d[3]);
+    if(d[3] && Hangul.isVowel(d[1]) && Hangul.isVowel(d[2])) {
+      var tmp = d[3];
+      d[3] = d[2];
+      d[2] = tmp;
+    }
+    return Hangul.assemble(d) ;
+  }).join('');
+}
+
+window.addEventListener("DOMContentLoaded", function() {
+  console.log("DOM fully loaded and parsed");
+  document.querySelector('.change').addEventListener('click', function() {
+    var changedText = translator(document.querySelector('.original-text').value);
+    document.querySelector('.result-text').innerText = changedText;
+  });
+});
